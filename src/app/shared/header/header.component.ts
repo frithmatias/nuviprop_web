04a2b5d249e5636d543f66c4666e92ceee9c3f5d
Matias Frith <frithmatias@gmail.com>
2020-02-07 11:13:09 -0300

    Se agrego menú en el HEADER para cambiar el THEME. Se hicieron correcciones generales en la maquetación.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { SidebarService, UsuarioService } from 'src/app/services/services.index';
+import { SidebarService, UsuarioService, SettingsService } from 'src/app/services/services.index';
 import { Usuario } from 'src/app/models/usuario.model';
 
 @Component({
@@ -15,14 +15,44 @@ export class HeaderComponent implements OnInit {
   constructor(
 	private router: Router,
 	private userService: UsuarioService,
-	private sidebarService: SidebarService
+  private sidebarService: SidebarService,
+  public _ajustes: SettingsService
   ) { }
 
   ngOnInit() {
-	this.publicmenu = this.sidebarService.publicHeaderMenu;
+    this.colocarCheck();
+  	this.publicmenu = this.sidebarService.publicHeaderMenu;
   }
 
   buscar(termino: string) {
 	this.router.navigate(['/buscar', termino]);
+  }
+
+
+  // THEME METHODS 
+  cambiarColor(tema: string, link: any) {
+    this.aplicarCheck(link);
+    this._ajustes.aplicarTema(tema);
+  }
+
+  // cuando selecciono un tema aplico el tilde
+  aplicarCheck(link: any) {
+    const selectores: any = document.getElementsByClassName('selector');
+    for (const ref of selectores) {
+      ref.classList.remove('working');
+    }
+    link.classList.add('working');
+  }
+
+  // cuando entro en la pagina account-settings aplico el tilde en el tema que esta seleccionado
+  colocarCheck() {
+    const selectores: any = document.getElementsByClassName('selector');
+    const tema = this._ajustes.ajustes.tema;
+    for (const ref of selectores) {
+      if (ref.getAttribute('data-theme') === tema) {
+        ref.classList.add('working');
+        break;
+      }
+    }
   }
 }
